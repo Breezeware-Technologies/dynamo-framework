675a8cccaf2c8234cad17dbe63522361130a6e71
Karthik Muthukumaraswamy <karthik@breezeware.net>
2020-09-03 23:00:14 -0400

    Fixed Checkstyle errors, PMD errors and updated Javadocs in Audit Lib module

--- dynamo-modules/dynamo-audit/dynamo-audit-lib/src/main/java/net/breezeware/dynamo/audit/controller/AuditManagementController.java
+++ dynamo-modules/dynamo-audit/dynamo-audit-lib/src/main/java/net/breezeware/dynamo/audit/controller/AuditManagementController.java
@@ -29,128 +29,124 @@ import net.breezeware.dynamo.util.usermgmt.CurrentUserDto;
 
 /**
  * Controller methods for audit log management.
- * 
  */
 @Controller
 @RequestMapping(value = "/audit/*")
 @PreAuthorize("hasAnyAuthority('ROLE_SYSTEM_ADMIN', 'ORGANIZATION_ADMIN')")
 public class AuditManagementController {
 
-	Logger logger = LoggerFactory.getLogger(AuditManagementController.class);
+    Logger logger = LoggerFactory.getLogger(AuditManagementController.class);
 
-	@Autowired
-	AuditService auditService;
+    @Autowired
+    AuditService auditService;
 
-	/**
-	 * 
-	 * Retrieves audit logs across all organizations. Accessible only by
-	 * SYSTEM_ADMIN role.
-	 * 
-	 * @param model
-	 * @param predicate
-	 * @param pageable
-	 * @param parameters
-	 * @return
-	 */
-	@RequestMapping(value = "/logs", method = RequestMethod.GET)
-	@PreAuthorize("hasAnyAuthority('ROLE_SYSTEM_ADMIN')")
-	public String listAuditLogs(Model model, @QuerydslPredicate(root = AuditItem.class) Predicate predicate,
-			@PageableDefault(sort = { "id" }, page = 0, size = 12) Pageable pageable,
-			@RequestParam MultiValueMap<String, String> parameters, HttpSession session) {
+    /**
+     * Retrieves audit logs across all organizations. Accessible only by
+     * SYSTEM_ADMIN role.
+     * @param model      the holder for Model attributes
+     * @param predicate  the interface for Boolean typed expressions. Supports
+     *                   binding of HTTP parameters to QueryDSL predicate
+     * @param pageable   the interface for pagination information
+     * @param parameters the holder for HTTP parameters in request
+     * @param session    the HTTPSession object
+     * @return a string to identify the Thymeleaf template
+     */
+    @RequestMapping(value = "/logs", method = RequestMethod.GET)
+    @PreAuthorize("hasAnyAuthority('ROLE_SYSTEM_ADMIN')")
+    public String listAuditLogs(Model model, @QuerydslPredicate(root = AuditItem.class) Predicate predicate,
+            @PageableDefault(sort = { "id" }, page = 0, size = 12) Pageable pageable,
+            @RequestParam MultiValueMap<String, String> parameters, HttpSession session) {
 
-		// logger.info("Predicate = {}", predicate.);
+        // logger.info("Predicate = {}", predicate.);
 
-		CurrentUserDto currentUserDto = (CurrentUserDto) session.getAttribute("currentUser");
+        CurrentUserDto currentUserDto = (CurrentUserDto) session.getAttribute("currentUser");
 
-		logger.info("Entering listAuditLogs()");
-		logger.info("# of params = " + parameters.size());
-		logger.info("user time zone = " + session.getAttribute("currentUser"));
-		logger.info("zone ID = " + currentUserDto.getUserTimeZoneId());
+        logger.info("Entering listAuditLogs()");
+        logger.info("# of params = " + parameters.size());
+        logger.info("user time zone = " + session.getAttribute("currentUser"));
+        logger.info("zone ID = " + currentUserDto.getUserTimeZoneId());
 
-		Page<AuditItem> pagedItems = auditService.findAllAuditItems(predicate, pageable);
-		logger.info("# of items fetched = {}", pagedItems.getNumberOfElements());
+        Page<AuditItem> pagedItems = auditService.findAllAuditItems(predicate, pageable);
+        logger.info("# of items fetched = {}", pagedItems.getNumberOfElements());
 
-		pagedItems = updateDateToClientTimeZone(pagedItems, currentUserDto.getUserTimeZoneId());
+        pagedItems = updateDateToClientTimeZone(pagedItems, currentUserDto.getUserTimeZoneId());
 
-		model.addAttribute("pagedItems", pagedItems);
-		model.addAttribute("activeNav", "audit");
-		logger.info("Leaving listAuditLogs()");
-		return "dynamo-audit/list-audit-logs";
-	}
+        model.addAttribute("pagedItems", pagedItems);
+        model.addAttribute("activeNav", "audit");
+        logger.info("Leaving listAuditLogs()");
+        return "dynamo-audit/list-audit-logs";
+    }
 
-	/**
-	 * 
-	 * Retrieves audit logs for a single organization.
-	 * 
-	 * @param model
-	 * @param predicate
-	 * @param pageable
-	 * @param parameters
-	 * @return
-	 */
-	@RequestMapping(value = "/orgLogs", method = RequestMethod.GET)
-	public String listOrganizationAuditLogs(Model model, @QuerydslPredicate(root = AuditItem.class) Predicate predicate,
-			@PageableDefault(sort = { "id" }, page = 0, size = 12) Pageable pageable,
-			@RequestParam MultiValueMap<String, String> parameters, HttpSession session) {
+    /**
+     * Retrieves audit logs for a single organization.
+     * @param model      the holder for Model attributes
+     * @param predicate  the interface for Boolean typed expressions. Supports
+     *                   binding of HTTP parameters to QueryDSL predicate
+     * @param pageable   the interface for pagination information
+     * @param parameters the holder for HTTP parameters in request
+     * @param session    the HTTPSession object
+     * @return a string to identify the Thymeleaf template
+     */
+    @RequestMapping(value = "/orgLogs", method = RequestMethod.GET)
+    public String listOrganizationAuditLogs(Model model, @QuerydslPredicate(root = AuditItem.class) Predicate predicate,
+            @PageableDefault(sort = { "id" }, page = 0, size = 12) Pageable pageable,
+            @RequestParam MultiValueMap<String, String> parameters, HttpSession session) {
 
-		// logger.info("Predicate = {}", predicate.);
-		CurrentUserDto currentUserDto = (CurrentUserDto) session.getAttribute("currentUser");
+        // logger.info("Predicate = {}", predicate.);
+        CurrentUserDto currentUserDto = (CurrentUserDto) session.getAttribute("currentUser");
 
-		logger.info("Entering listAuditLogs()");
-		logger.info("# of params = " + parameters.size());
-		logger.info("zone ID = " + currentUserDto.getUserTimeZoneId());
+        logger.info("Entering listAuditLogs()");
+        logger.info("# of params = " + parameters.size());
+        logger.info("zone ID = " + currentUserDto.getUserTimeZoneId());
 
-		long organizationId = Long.valueOf(currentUserDto.getOrganizationId());
+        long organizationId = Long.valueOf(currentUserDto.getOrganizationId());
 
-		Page<AuditItem> pagedItems = auditService.findAuditItemsForOrganization(organizationId, predicate, pageable);
-		logger.info("# of items fetched = {}", pagedItems.getNumberOfElements());
+        Page<AuditItem> pagedItems = auditService.findAuditItemsForOrganization(organizationId, predicate, pageable);
+        logger.info("# of items fetched = {}", pagedItems.getNumberOfElements());
 
-		pagedItems = updateDateToClientTimeZone(pagedItems, currentUserDto.getUserTimeZoneId());
+        pagedItems = updateDateToClientTimeZone(pagedItems, currentUserDto.getUserTimeZoneId());
 
-		model.addAttribute("pagedItems", pagedItems);
-		model.addAttribute("activeNav", "audit");
+        model.addAttribute("pagedItems", pagedItems);
+        model.addAttribute("activeNav", "audit");
 
-		// model.addAttribute("locale", Locale.US);
+        // model.addAttribute("locale", Locale.US);
 
-		logger.info("Leaving listAuditLogs()");
-		return "dynamo-audit/list-audit-logs";
-	}
+        logger.info("Leaving listAuditLogs()");
+        return "dynamo-audit/list-audit-logs";
+    }
 
-	/**
-	 * 
-	 * Gets the id of the audit log and re-directs to the view-audit log page.
-	 * 
-	 * @param id
-	 * @param model
-	 * @return
-	 */
-	@RequestMapping(value = "/logs", method = RequestMethod.GET, params = { "id" })
-	public String viewAuditLog(@RequestParam("id") long id, Model model) {
+    /**
+     * Gets the id of the audit log and re-directs to the view-audit log page.
+     * @param id    the ID of the AuditItem that is requested
+     * @param model the holder for Model attributes
+     * @return a string to identify the Thymeleaf template
+     */
+    @RequestMapping(value = "/logs", method = RequestMethod.GET, params = { "id" })
+    public String viewAuditLog(@RequestParam("id") long id, Model model) {
 
-		logger.info("Entering viewAuditLog()");
-		AuditItem auditItem = auditService.getItemById(id);
+        logger.info("Entering viewAuditLog()");
+        AuditItem auditItem = auditService.getItemById(id);
 
-		model.addAttribute("auditItem", auditItem);
-		model.addAttribute("activeNav", "audit");
-		logger.info("Leaving viewAuditLog()");
-		return "dynamo-audit/view-audit-log";
-	}
+        model.addAttribute("auditItem", auditItem);
+        model.addAttribute("activeNav", "audit");
+        logger.info("Leaving viewAuditLog()");
+        return "dynamo-audit/view-audit-log";
+    }
 
-	private Page<AuditItem> updateDateToClientTimeZone(Page<AuditItem> pagedItems, String zoneId) {
+    private Page<AuditItem> updateDateToClientTimeZone(Page<AuditItem> pagedItems, String zoneId) {
 
-		DateTimeFormatter formatter = null;
+        DateTimeFormatter formatter = null;
+        if (zoneId != null && zoneId.length() > 0) {
+            // format dates according to user
+            formatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM).withZone(ZoneId.of(zoneId));
+        } else {
+            formatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM).withZone(ZoneId.of("UTC"));
+        }
 
-		if (zoneId != null && zoneId.length() > 0) {
-			// format dates according to user
-			formatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM).withZone(ZoneId.of(zoneId));
-		} else {
-			formatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM).withZone(ZoneId.of("UTC"));
-		}
+        for (AuditItem item : pagedItems) {
+            item.setAuditDateString(formatter.format(item.getAuditDate()));
+        }
 
-		for (AuditItem item : pagedItems) {
-			item.setAuditDateString(formatter.format(item.getAuditDate()));
-		}
-
-		return pagedItems;
-	}
+        return pagedItems;
+    }
 }
