1eb22d84598e7548f36055c030e2f619af57b0d8
Deepak Palanisamy <deepak@breezeware.net>
2021-01-21 10:46:43 +0530

    added querydsl support for inventory item

--- dynamo-modules/dynamo-inventory/src/main/java/net/breezeware/dynamo/inventory/dao/InventoryItemRepository.java
+++ dynamo-modules/dynamo-inventory/src/main/java/net/breezeware/dynamo/inventory/dao/InventoryItemRepository.java
@@ -1,13 +1,24 @@
 package net.breezeware.dynamo.inventory.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.stereotype.Repository;
 
 import net.breezeware.dynamo.inventory.entity.InventoryItem;
+import net.breezeware.dynamo.inventory.entity.QInventoryItem;
 
 @Repository
-public interface InventoryItemRepository extends JpaRepository<InventoryItem, Long> {
+public interface InventoryItemRepository extends JpaRepository<InventoryItem, Long>,
+        QuerydslPredicateExecutor<InventoryItem>, QuerydslBinderCustomizer<QInventoryItem> {
 
     InventoryItem findByItemSku(String itemSku);
+
+    @Override
+    default void customize(QuerydslBindings bindings, QInventoryItem root) {
+        // TODO Auto-generated method stub
+
+    }
 
 }
