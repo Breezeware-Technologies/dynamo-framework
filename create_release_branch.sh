4bdc1a1db14306c31c72aa45fac314940ee61087
Karthik Muthukumaraswamy <karthik@breezeware.net>
2021-03-09 13:12:25 -0500

    updated to version 2.1.3-SNAPSHOT

--- create_release_branch.sh
+++ create_release_branch.sh
@@ -7,5 +7,6 @@ git checkout -b release/$NEW_RELEASE_BRANCH_NAME
 # change any file
 read -p "Enter new development version (For Example: 0.0.2-SNAPSHOT): " NEW_VERSION 
 echo NEW_VERSION | mvn release:update-versions -DautoVersionSubmodules=true
+git add .
 git commit -am "updated to version $NEW_VERSION"
 git push origin release/$NEW_RELEASE_BRANCH_NAME
