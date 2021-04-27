54d128053f24dae3c2a5172a9ea63ca5ad486b57
Guru VedhaRamanan <guru@breezeware.net>
2021-04-27 17:03:40 +0530

    Removed Unused java entity

--- dynamo-modules/dynamo-organization/dynamo-organization-lib/src/main/java/net/breezeware/dynamo/organization/messaging/UserCreatedMessage.java
+++ dynamo-modules/dynamo-organization/dynamo-organization-lib/src/main/java/net/breezeware/dynamo/organization/messaging/UserCreatedMessage.java
@@ -2,10 +2,10 @@ package net.breezeware.dynamo.organization.messaging;
 
 import java.io.Serializable;
 
+import lombok.Data;
 import net.breezeware.dynamo.organization.entity.Organization;
 import net.breezeware.dynamo.organization.entity.User;
-
-import lombok.Data;
+import net.breezeware.dynamo.rabbitmq.entity.RabbitMqMessage;
 
 @Data
 public class UserCreatedMessage extends RabbitMqMessage implements Serializable {
